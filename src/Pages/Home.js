4a8782dcbedc367c7fb84a9e460b024b84029502
nurdin73 <nurdin.reverse73@gmail.com>
2019-12-10 00:46:45 +0700

    Add Category slide

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
-import Header from '../Component/Header'
-// import Category from '../Component/Category'
-import Feature from '../Component/Feature'
-import Article from '../Component/Article'
-import Popular from '../Component/Popular'
-import { Container, Grid } from '@material-ui/core';
+import React, {Component} from 'react';
+import Header from '../Component/Header';
+import Category from '../Component/Category'
+import Feature from '../Component/Feature';
+import Article from '../Component/Article';
+import Popular from '../Component/Popular';
+import {Container, Grid} from '@material-ui/core';
 class Home extends Component {
-  render() {
+  render () {
     return (
       <div>
-        <Header/>
-				<Container style={{marginTop:20}}>
-					<Feature/>
-				</Container>
-				<Container style={{marginTop:40}}>
-					<div className="container-app">
-						<Grid container spacing={8}>
-							<Grid item xs={12} md={8}>
-								<Article/>
-								<Article/>
-								<Article/>
-							</Grid>
-							<Grid item xs={12} md={4}>
-								<Popular/>
-							</Grid>
-						</Grid>
-					</div>
-				</Container>
+        <Header />
+		<div className="container-app">
+			<Category/>
+		</div>
+        <Container style={{marginTop: 20}}>
+          <Feature />
+        </Container>
+        <Container style={{marginTop: 40}}>
+          <div className="container-app">
+            <Grid container spacing={8}>
+              <Grid item xs={12} md={8}>
+                <Article />
+                <Article />
+                <Article />
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Popular />
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
       </div>
     );
   }
